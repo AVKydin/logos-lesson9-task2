a80d5ffd608b53b69a1d22d04f1cab32858462a0
AVKydin <tlalok@ukr.net>
2022-11-09 00:19:31 +0200

    finish day

--- main.js
+++ main.js
@@ -26,6 +26,94 @@
 //  - status
 //  - resizable;
 
-document.querySelector("#close").addEventListener("click", () => {
-  window.close("https://terrikon.com/uk/", "Terrikon", "width=400, height=400, left=400, top=200, resizable=yes");
-});
+// let popup;
+// document.querySelector("#open").addEventListener("click", () => {
+//   popup = window.open("https://terrikon.com", "Terrikon", "width=400, height=400, left=400, top=200");
+// });
+
+// function closeWindow() {
+//     popup.close();
+// }
+// setTimeout(closeWindow, 2000);
+
+// let popup;
+// document.querySelector("#open").addEventListener("click", () => {
+//   popup = window.open("https://terrikon.com", "Terrikon", "width=400, height=400, left=400, top=200");
+// });
+// document.querySelector("#close").addEventListener("click", () => {
+//   popup.close();
+// });
+
+
+// let popup;
+// document.querySelector("#open").addEventListener("click", () => {
+//   popup = window.open("https://terrikon.com", "Terrikon", "width=400, height=400, left=400, top=200");
+//   popup.resizeTo(500, 300);
+// });
+
+
+// document.write(`кiлькфсть сторінок історіїї = ${window.history.length}`)
+// history.back();
+// history.forward();
+// history.go(-3);
+// history.go(4);
+
+
+// document.write(`<h2>рядок запиту ${location.href}</h2>`);
+// document.write(`<h2>шлях до ресурсу ${location.pathname}</h2>`);
+// document.write(`<h2>загальна схума запиту ${location.origin}</h2>`);
+// document.write(`<h2>протокол передачі ${location.protocol}</h2>`);
+// document.write(`<h2>порт ${location.port}</h2>`);
+// document.write(`<h2>ім'я хоста ${location.host}</h2>`);
+// document.write(`<h2>хеш ${location.hash}</h2>`);
+// document.write(`<h2>пошук ${location.search}</h2>`);
+
+
+// location = 'htpps://www.google.com';
+// location.assign('htpps://www.google.com');
+
+
+// location.reload(forceReload);
+// location.replace(url);
+
+
+// navigator
+
+// document.write(navigator.userAgent);
+// let browser;
+// let uAgent = navigator.userAgent;
+// if (uAgent.indexOf('Chrome' > -1)) {
+//   browser = 'Google chrom';
+// }
+
+// document.write(browser);
+
+
+// function success(position) {
+//   let latitude = position.coords.latitude;
+//   let longitude = position.coords.longitude;
+//   let altitude = position.coords.altitude;
+//   let speed = position.coords.speed;
+//   document.write(`<h2>широта ${latitude} довгота ${longitude} висота ${altitude} швидкість ${speed}</h2>`);
+// }
+
+// function error() {
+//   document.write(`<h2>помилка при визначенні місцязнаходження</h2>`)
+// }
+
+// navigator.geolocation.getCurrentPosition(success, error);
+
+
+// window.addEventListener('scroll', function () {
+//   // console.log(event);
+//   // document.body.style.background = `rgb(0,${window.scrollY},${window.scrollY})`;
+//   if (window.scrollY < 250) {
+//   console.log(window.scrollY);
+
+//     document.body.style.background = `rgb(0,${window.scrollY},${window.scrollY})`;
+//   }
+// })
+
+
+window.scroll(0, 1000)
+
