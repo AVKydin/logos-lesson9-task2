97b816bab25deea1d7fab99673f72402bade3629
AVKydin <tlalok@ukr.net>
2022-11-08 17:56:49 +0200

    start

---
+++ main.js
@@ -0,0 +1,31 @@
+// alert('hello js');
+
+// window.alert("hello js");
+
+// let alert = function (message) {
+//     document.write(`sdfdsf: ${message}`);
+// }
+// window.alert("hello js");
+
+// var message = 'hello js';
+// document.write(window.message);
+
+// window.open('https://www.football24.ua', 'Football24', 'width=400, height=40, resizable=yes');
+// document.querySelector("#open").addEventListener("click", () => {
+//   window.open("https://terrikon.com/uk/", "Terrikon", "width=400, height=400, left=400, top=200, resizable=yes");
+// })
+  // -width;
+  // -height;
+  // -left
+  // -top
+//  - titlebar
+//  - menubar
+//  - toolbar
+//  - location
+//  - scrollbars
+//  - status
+//  - resizable;
+
+document.querySelector("#close").addEventListener("click", () => {
+  window.close("https://terrikon.com/uk/", "Terrikon", "width=400, height=400, left=400, top=200, resizable=yes");
+});
